22173c483de286601ed3d72d79b330f0fae9fff9
吴乐川 <wulechuan@live.com>
2022-04-28 16:50:14 +0800

    终于恢复了旧日模样（指代码实现的功能，非指代码本身）。

--- 文档集/示范应用集/示范应用-1/采用-typescript-与-stylus-编写/src/视图/页面/示范页1.vue
+++ 文档集/示范应用集/示范应用-1/采用-typescript-与-stylus-编写/src/视图/页面/示范页1.vue
@@ -15,8 +15,7 @@
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
 
-import WlcEcharts, {
-} from '@wulechuan/echarts-vue2-component'
+import WlcEcharts from '@wulechuan/echarts-vue2-component'
 
 import * as echarts from 'echarts/core'
 
