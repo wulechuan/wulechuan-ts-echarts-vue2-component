5b37c6653bb4fd0f98a92cd3c7e30367cce5b2ad
吴乐川 <wulechuan@live.com>
2022-07-09 11:33:24 +0800

    第 2.0.5 版。发布文件集排除 gulpfile* 。

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-import buildAndWatch from './gulpfile.esm.js/index.js'
+import buildAndWatch from './gulp-任务集/index.js'
 
-export * from './gulpfile.esm.js/index.js'
+export * from './gulp-任务集/index.js'
 export default buildAndWatch
